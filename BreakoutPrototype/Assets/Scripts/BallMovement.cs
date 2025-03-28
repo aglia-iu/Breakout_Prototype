dd0c5b974f441ff7f154cf3e40e8c6d22119c786
Anjali <agali1@iastate.edu>
2025-03-27 22:52:59 -0500

    moved player movement to seperate function

--- BreakoutPrototype/Assets/Scripts/BallMovement.cs
+++ BreakoutPrototype/Assets/Scripts/BallMovement.cs
@@ -7,6 +7,8 @@ public class BallMovement : MonoBehaviour
 {
     //PRIVATE VARIABLES
     private Rigidbody rigidBody;
+    private bool shieldHit = false;
+    private bool wallHit = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,12 +18,51 @@ public class BallMovement : MonoBehaviour
         {
             Debug.Log("This component does not have an attached rigidbody.");
         }
+        //Vector3 forceToAdd = new Vector3(0.0f, 0.0f, -1.0f);
+        rigidBody.AddForce(ForceVector(-1.0f));
+
+        shieldHit = false;
+        wallHit = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 forceToAdd = new Vector3(0.0f, 0.0f, -1.0f);
-        rigidBody.AddForce (forceToAdd);
+        ActivateForce();
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "shield")
+        {
+            shieldHit = true;
+            wallHit = false;
+        }
+
+        if (collider.gameObject.tag == "yellowWall" || collider.gameObject.tag == "redWall")
+        {
+            shieldHit = false;
+            wallHit = true;
+        }
+
+    }
+
+    private void ActivateForce()
+    {
+        Debug.Log("Shield: " + shieldHit + ", Wall: " + wallHit);
+        if (shieldHit)
+        {
+            rigidBody.AddForce(ForceVector(1.0f));
+        }
+        else if (wallHit)
+        {
+            rigidBody.AddForce(ForceVector(-1.0f));
+
+        }
+    }
+
+    private Vector3 ForceVector(float zVal)
+    {
+        return new Vector3 (0.0f, 0.0f, zVal);
     }
 }
--- BreakoutPrototype/Assets/Scripts/PlayerController.cs
+++ BreakoutPrototype/Assets/Scripts/PlayerController.cs
@@ -10,7 +10,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 0; // The speed of the character
     
     //PRIVATE VARIABLES:
-    private Vector3 playerStart; // The start position of the player
+    private Vector3 playerStartPos; // The start position of the player
+    private Quaternion playerStartRot; // The start position of the player
     private float movementX; // The movement of the character along the X axis.
     private Rigidbody rigidBody; // The movement of the character along the X axis.
 
@@ -18,27 +19,39 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>(); // The rigidbody of the character to add force to the character.
-        playerStart = this.transform.position; // The starting position of the player.
-
+        playerStartPos = this.transform.position; // The starting position of the player.
+        playerStartRot = this.transform.rotation; // The starting rotation of the player
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(movementPlayer());
+        MovementPlayer();
+        LockPlayerOnAxis();
 
     }
 
-    public string movementPlayer()
+    public void MovementPlayer()
     {
         movementX = Input.GetAxis("Horizontal");
-        Vector3 movementAlongX = new Vector3(movementX, 0, 0);
+        Vector3 movementAlongX = new Vector3(movementX * speed, 0, 0);
 
         // Add the provided force to the rigidBody
-        Debug.Log(movementX);
         rigidBody.AddForce(movementAlongX);
 
-        return ("Character Position is: "  + this.transform.position.ToString());
+    }
+
+    public string LockPlayerOnAxis()
+    {
+        // Set the x and z component of the object to 0.0f.
+        //this.rigidBody.;
+        this.rigidBody.constraints = 
+            RigidbodyConstraints.FreezePositionY |
+            RigidbodyConstraints.FreezePositionZ | 
+            RigidbodyConstraints.FreezeRotation;
+
+        
+        return "position and rotation set";
     }
 }
